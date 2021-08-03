69faee70e514a7072173b2c4e8173060edc8864b
SiddamsettySahithi <siddamsetty.sahithi@capgemini.com>
2021-08-03 13:14:42 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-args="--regex --entropy=False --max_depth=50" # Default trufflehog options
+args="--regex --no-entropy --max_depth=50" # Default trufflehog options
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
