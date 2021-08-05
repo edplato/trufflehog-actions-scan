16403076d776076a21a126086bd65528fd0dcfb8
SiddamsettySahithi <siddamsetty.sahithi@capgemini.com>
2021-08-05 12:20:34 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
-#set -e # Abort script at first error
+set -e # Abort script at first error
 
-#args="--regex --no-entropy" # Default trufflehog options
+args="--regex --entropy=False --max_depth=50" # Default trufflehog options
+
+if [ -n "${INPUT_SCANARGUMENTS}" ]; then
+  args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
+fi
 
 # By default the 'WORKDIR' of our Docker image is set to the 'GITHUB_WORKSPACE'
-# which is mounted into our image. This means, as long as a checkout action was
+# which is mounted into our image. This means, as long as a checkout action was
 # done before our action runs, we'll have access to the repository.
 githubRepo="file://$(pwd)" # Default target repository
-#query="$args $githubRepo" # Build args query with repository url
-trufflehog3 $githubRepo
+query="$args $githubRepo" # Build args query with repository url
+trufflehog $query
